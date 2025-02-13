808666fc6554413e2d7b3dae8ad5a22d33276b3a
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-02-13 15:44:41 +0800

    feat(transformers): debug auto image processor

--- mindone/transformers/models/auto/__init__.py
+++ mindone/transformers/models/auto/__init__.py
@@ -1,2 +1,2 @@
-from .modeling_auto import AutoModel, AutoModelForCausalLM, AutoModelForMaskedLM
 from .image_processing_auto import AutoImageProcessor
+from .modeling_auto import AutoModel, AutoModelForCausalLM, AutoModelForMaskedLM
